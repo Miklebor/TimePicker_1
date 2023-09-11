c886b4dafc92b94f5bf14bc721c2a0eff1fb346b
Mike <konakovskiy@gmail.com>
2023-09-11 23:22:00 +0300

    Эксперименты с TextClock формат вывода даты

--- app/src/main/java/com/example/timepicker_1/MainActivity.kt
+++ app/src/main/java/com/example/timepicker_1/MainActivity.kt
@@ -17,49 +17,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val dateSelBt = findViewById<Button>(R.id.btSelectDate)
         val timeSelBt = findViewById<Button>(R.id.btSelectTime)
         //val txtClock = findViewById<TextClock>(R.id.textClock)
         val txtView = findViewById<TextView>(R.id.textView)
         //val timeClk = findViewById<TextClock>(R.id.textClock)
 
                 timeSelBt.setOnClickListener {
-                //currentTimePicker()
-                   // writeDateTime()
                     randomTimePicker()
-                    //DatePickerDialog()
-        }
+                }
+                dateSelBt.setOnClickListener {
+                    datePickerDialog()
+                }
+
+
     }
     @SuppressLint("SetTextI18n")
     private fun randomTimePicker() {
         val c = Calendar.getInstance()
         val hour = c.get(Calendar.HOUR)
         val minute = c.get(Calendar.MINUTE)
-        val day = c.get(Calendar.DAY_OF_MONTH)
-        val month = c.get(Calendar.MONTH)
-        val year = c.get(Calendar.YEAR)
         val timeTextEd = findViewById<EditText>(R.id.editTextTime)
 
         val tpd = TimePickerDialog(this,TimePickerDialog.OnTimeSetListener(function = { view, h, m ->
             /*Toast.makeText(this, h.toString() + " : " + m , Toast.LENGTH_LONG).show()*/
             timeTextEd.setText("$h:$m")
-        }),hour,minute,false)
+        }),hour,minute,true)
 
         tpd.show()
-
     }
-    private fun DatePickerDialog(mainActivity: MainActivity, onDateSetListener: DatePickerDialog.OnDateSetListener) {
+
+    private fun datePickerDialog() {
         val c = Calendar.getInstance()
         val day = c.get(Calendar.DAY_OF_MONTH)
         val month = c.get(Calendar.MONTH)
         val year = c.get(Calendar.YEAR)
         val dateTextEd = findViewById<EditText>(R.id.editTextDate)
-
-            val dpd = DatePickerDialog(this,)
-
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            dateTextEd.run {"$dayOfMonth-${monthOfYear + 1}-$year"}
+            }, year, month, day)
         dpd.show()
-
-        return //TODO("Provide the return value")
     }
 
     @SuppressLint("SetTextI18n")
@@ -85,3 +82,6 @@ class MainActivity : AppCompatActivity() {
         timeTextEd.setText("${txtClock?.text}")
     }
 }
+/*Toast.makeText(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG).show() */
+//dateTextEd.setText("")
+/*Toast.makeText(this, h.toString() + " : " + m , Toast.LENGTH_LONG).show()*/
